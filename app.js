f20e0756091438ecd6ee38767e593f7868c11383
DamienDcz <damiendescroizette@gmail.com>
2021-10-10 21:32:26 +0200

    Revert "Mise en place front - récupération depuis Prismic"
    This reverts commit 1f9570f6739b1f716a517ea1e39b9e52403d8562.

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 require('dotenv').config()
 const express = require('express')
-const errorHandler = require('errorhandler')
-const logger = require('morgan')
-const methodOverride = require('method-override')
 const path = require('path')
 const app = express()
 const port = 3000
-
-// MiddleWare
-app.use(logger('dev'))
-app.use(errorHandler())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride())
 
 const Prismic = require('@prismicio/client')
 const PrismicDOM = require('prismic-dom')
@@ -26,101 +16,58 @@ const initApi = (req) => {
 
 // Link Resolver
 const handleLinkResolver = (doc) => {
-  if (doc.type === 'product') {
-    return `/detail/${doc.slug}`
-  }
+  // Define the url depending on the document type
+  // if (doc.type === 'page') {
+  //   return '/page/' + doc.uid;
+  // } else if (doc.type === 'blog_post') {
+  //   return '/blog/' + doc.uid;
+  // }
 
-  if (doc.type === 'about') {
-    return '/about'
-  }
-
-  if (doc.type === 'collections') {
-    return '/collections'
-  }
-
+  // Default to homepage
   return '/'
 }
 
 // Middleware to inject prismic context
 app.use((req, res, next) => {
-  res.locals.Link = handleLinkResolver
-  res.locals.PrismicDOM = PrismicDOM
-  res.locals.Numbers = index => {
-    return index === 0 ? 'One' : index === 1 ? 'Two' : index === 2 ? 'Three' : index === 3 ? 'Four' : ''
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver
   }
+  // add PrismicDOM in locals to access them in templates.
+  res.locals.PrismicDOM = PrismicDOM
   next()
 })
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-// Default values for each page
-const handleRequest = async api => {
-  const meta = await api.getSingle('meta')
-  const navigation = await api.getSingle('navigation')
-  const preloader = await api.getSingle('preloader')
-  return {
-    meta,
-    navigation,
-    preloader
-  }
-}
-
-app.get('/', async (req, res) => {
-  const api = await initApi(req)
-  const home = await api.getSingle('home')
-  const defaults = await handleRequest(api)
-
-  const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
-    fetchLinks: 'product.image'
-  })
-  res.render('pages/home', {
-    ...defaults,
-    collections,
-    home
-  })
+app.get('/', (req, res) => {
+  res.render('pages/home')
 })
 
-app.get('/about', async (req, res) => {
-  const api = await initApi(req)
-  const defaults = await handleRequest(api)
-  const about = await api.getSingle('about')
-  res.render('pages/about', {
-    ...defaults,
-    about
+app.get('/about', (req, res) => {
+  initApi(req).then(api => {
+    api.query(
+      Prismic.Predicates.any('document.type', ['about', 'meta'])
+    ).then(response => {
+      const { results } = response
+      const [about, meta] = results
+      res.render('pages/about', {
+        about,
+        meta
+      })
+    })
   })
 
   // res.render('pages/about')
 })
 
-app.get('/collections', async (req, res) => {
-  const api = await initApi(req)
-  const defaults = await handleRequest(api)
-  const home = await api.getSingle('home')
-
-  const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
-    fetchLinks: 'product.image'
-  })
-
-  res.render('pages/collections', {
-    ...defaults,
-    home,
-    collections
-  })
+app.get('/collection', (req, res) => {
+  res.render('pages/collection')
 })
 
-app.get('/detail/:uid', async (req, res) => {
-  const api = await initApi(req)
-  const defaults = await handleRequest(api)
-  const product = await api.getByUID('product', req.params.uid, {
-    fetchLinks: 'collection.title'
-  })
-  res.render('pages/detail', {
-    ...defaults,
-    product
-  })
-
-  // res.render('pages/detail')
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail')
 })
 
 app.listen(port, () => {
